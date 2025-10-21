dc8752fb9a89830b70be22e68522092fbda346bb
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-10-21 15:29:53 +0545

    feat(utils): add cowboy helper to warn when holding movement keys

--- lua/goodies/utils.lua
+++ lua/goodies/utils.lua
@@ -2,6 +2,7 @@
 -- %s Date: 2024-05-23
 -- %s GitHub: https://github.com/rubiin
 -- %s Twitter: https://twitter.com/RubinCodes
+
 -- These are several commands that are too small to be created as standalone plugins,
 -- but too large to be included directly in the main configuration file, where they
 -- might clutter the actual configuration. Each function is self-contained, apart from
@@ -274,6 +275,43 @@ function M.open_at_regex_101()
 	M.open_in_browser(url)
 end
 
+
+local M = {}
+
+function M.cowboy()
+  ---@type table?
+  local id
+  local ok = true
+  for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
+    local count = 0
+    local timer = assert(vim.loop.new_timer())
+    local map = key
+    vim.keymap.set("n", key, function()
+      if vim.v.count > 0 then
+        count = 0
+      end
+      if count >= 10 then
+        ok, id = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
+          icon = "🤠",
+          replace = id,
+          keep = function()
+            return count >= 10
+          end,
+        })
+        if not ok then
+          id = nil
+          return map
+        end
+      else
+        count = count + 1
+        timer:start(2000, 0, function()
+          count = 0
+        end)
+        return map
+      end
+    end, { expr = true, silent = true })
+  end
+end
 
 
 return M
